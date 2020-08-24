eead2997aae7ce82f8a2dc57b477fc2128127dbf
oka <pn11@users.noreply.github.com>
2020-08-24 17:05:25 +0900

    migrate nextcloud from macOS to debian

---
+++ docker/nextcloud/volumes/nextcloud_data/config/config.php
@@ -0,0 +1,43 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'occyekoxomf2',
+  'passwordsalt' => '7+gjGMZ5UrKuWAxS9dz8/jr8ek4DGX',
+  'secret' => 'KZxjJN0UduROQRzx912g2/8O+p7WvHoCA1RQsuvC9zlNbYP0',
+  'trusted_domains' => 
+  array (
+    0 => 'nextcloud.dip.jp:37498',
+    1 => '192.168.*.*',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '16.0.11.1',
+  'overwrite.cli.url' => 'http://nextcloud.dip.jp:37498',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'djskl312FJAleij',
+  'installed' => true,
+  'theme' => '',
+  'loglevel' => 2,
+  'maintenance' => false,
+);
